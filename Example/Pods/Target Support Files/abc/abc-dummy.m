77e070d896e7f24469212354f3c7757c0bae58bd
myoungin86 <myoungin@tpmn.io>
2023-12-08 09:36:35 +0900

    0.1.1

---
+++ Example/Pods/Target Support Files/abc/abc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_abc : NSObject
+@end
+@implementation PodsDummy_abc
+@end
--- Example/abc/ViewController.swift
+++ Example/abc/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import abc
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        abc.Foma.sayHello(with: "krysiel86")
+        
     }
-
+  
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+ 
 }
 
